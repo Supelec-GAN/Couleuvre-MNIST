4776f2381e3f709592d3a851d5f95a43273c68fc
Manu-Tran <emmanuel.tran@gmail.com>
2017-11-02 11:03:54 +0100

    Paramétrage du main.cpp -> Crétation des Batch

--- sources/main.cpp
+++ sources/main.cpp
@@ -15,20 +15,41 @@ int main()
     try
     {
         // Construction du réseau de neurones
-        std::vector<unsigned int> sizes{ {2,2,2,1} };
-        std::vector<Functions::ActivationFun> funs{ {Functions::sigmoid(3.f), Functions::sigmoid(3.f), Functions::sigmoid(3.f)} };
+        std::vector<unsigned int> sizes{ {784,1000,300,10} };
+        std::vector<Functions::ActivationFun> funs{ {Functions::sigmoid(0.1f), Functions::sigmoid(0.1f), Functions::sigmoid(0.1f)} };
         std::shared_ptr<NeuralNetwork> network(new NeuralNetwork(sizes, funs));
 
-        mnist_reader reader("MNIST/test-images-10k", "MNIST/test-labels-10k");
+        mnist_reader readerTrain("MNIST/train-images-60k", "MNIST/train-labels-60k");
+        std::vector<Eigen::VectorXf> imageTrain;
+        Eigen::VectorXi labelTrain;
+        readerTrain.ReadMNIST(imageTrain, labelTrain);
 
-        std::vector<Eigen::VectorXf> v;
-        Eigen::VectorXi i;
-        reader.ReadMNIST(v, i);
+        mnist_reader readerTest("MNIST/test-images-10k", "MNIST/test-labels-10k");
+        std::vector<Eigen::VectorXf> imageTest;
+        Eigen::VectorXi labelTest;
+        readerTest.ReadMNIST(imageTest, labelTest);
+
+        Application::Batch batchTrain;
+        Application::Batch batchTest;
+
+        for(auto i(0); i< labelTrain.size(); i++)
+        {
+            Eigen::MatrixXf outputTrain = Eigen::MatrixXf::Zero(10,1);
+            outputTrain(labelTrain(i)) = 1;
+            batchTrain.push_back(Application::Sample(imageTrain[i], outputTrain));
+        }
+        cout << "Chargement du Batch d'entrainement effectué !" << endl;
+        for(auto i(0); i< labelTest.size(); i++)
+        {
+            Eigen::MatrixXf outputTest = Eigen::MatrixXf::Zero(10,1);
+            outputTest(labelTest(i)) = 1;
+            batchTest.push_back(Application::Sample(imageTest[i], outputTest));
+        }
+        cout << "Chargement du Batch de test effectué !" << endl;
 
         //Construction de l'application qui gère tout
-        //Application appXOR(network, teachBatch, testBatch);
-
-        //appXOR.runExperiments(300, 100, 1000);
+        Application appMNIST(network, batchTrain, batchTest);
+        appMNIST.runExperiments(12, 100, 400);
     }
     catch (const std::exception& ex)
     {
